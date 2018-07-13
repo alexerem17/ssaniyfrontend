3dbdffad76a392e7a9f5301f242ddd6c7b04d1a9
Alexander Eremenko <alexerem17@gmail.com>
2018-07-13 18:30:04 +0300

    Third ssaniy commit

--- My project/first/plugins/mixins.js
+++ My project/first/plugins/mixins.js
@@ -2,9 +2,11 @@ import Vue from 'Vue'
 
 Vue.mixin({
     methods: {
-      normal: function (ymd) {
-      var time= new Date();
-      var ymd=time.split('-') 
+      normal: function (oDate) {
+      var iYear = oDate.getFullYear();
+      var iMonth = oDate.getMounth();
+      var iDay = oDate.getDate();
+      return `${iYear}-${(iMonth >= 10 ? iMonth: `0${iMonth}`)}-${(iDay >= 10 ? iDay : `0${iDay}`)}`
       }
     }
   })
